9a473f637d6fdb688b241037aae91a4dc69b22a6
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-11 13:48:25 +0200

    Fix: update modules, merge opts, set pipeline, and astro to v2.0.10

--- dist/options/index.js
+++ dist/options/index.js
@@ -1,1 +1,4 @@
-import e from"files-pipeline/dist/lib/deepmerge.js";import i from"files-pipeline/dist/options/index.js";var p=e(i,{pipeline:{}});export{p as default};
+import e from "files-pipeline/dist/lib/deepmerge.js";
+import i from "files-pipeline/dist/options/index.js";
+var p=e(i,{pipeline:{}});export { p as default };
+
