a9c99a881106561a0bb6df4a8ffd2ea1a77138ce
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-14 00:42:25 +0200

    squash!

--- dist/index.js
+++
@@ -1,4 +0,0 @@
-import a from "files-pipeline/dist/lib/deepmerge.js";
-import o from "./options/index.js";
-var p=(t={})=>{for(const e in t)Object.prototype.hasOwnProperty.call(t,e)&&t[e]===!0&&(t[e]=o[e]);t=a(o,t);const r=new Set;if(typeof t.path<"u"&&(t.path instanceof Array||t.path instanceof Set))for(const e of t.path)r.add(e);return{name:"astro-dead-links",hooks:{"astro:build:done":async({dir:e})=>{r.size||r.add(e)}}}};export { p as default };
-
--- dist/options/index.js
+++
@@ -1,4 +0,0 @@
-import e from "files-pipeline/dist/lib/deepmerge.js";
-import i from "files-pipeline/dist/options/index.js";
-var p=e(i,{pipeline:{}});export { p as default };
-
