2ed9d5527c36cb50726bd5628127855c5bc626ef
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-25 17:20:46 +0200

    squash!

--- dist/options/index.js
+++ dist/options/index.js
@@ -1,1 +1,1 @@
-import e from"files-pipeline/dist/lib/deepmerge.js";import i from"files-pipeline/dist/options/index.js";var p=e(i,{pipe:{}});export{p as default};
+import e from"files-pipeline/dist/lib/deepmerge.js";import i from"files-pipeline/dist/options/index.js";var p=e(i,{pipeline:{}});export{p as default};
