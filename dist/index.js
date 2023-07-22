ddcfe2f46f97bd0b3bf70ccab525b9cbc95064e4
Nikola Hristov <nikola@nikolahristov.tech>
2023-07-22 07:38:33 +0300

    squash!

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,4 @@
-import i from"files-pipe/dist/lib/deepmerge.js";import n from"./options/index.js";var s=(o={})=>{for(const t in o)Object.prototype.hasOwnProperty.call(o,t)&&o[t]===!0&&(o[t]=n[t]);const e=i(n,o),r=new Set;if(typeof e.path<"u"&&(e.path instanceof Array||e.path instanceof Set))for(const t of e.path)r.add(t);return{name:"astro-dead-links",hooks:{"astro:build:done":async({dir:t})=>{r.size||r.add(t)}}}};export{s as default};
+import i from "files-pipe/dist/lib/deepmerge.js";
+import n from "./options/index.js";
+var s=(o={})=>{for(const t in o)Object.prototype.hasOwnProperty.call(o,t)&&o[t]===!0&&(o[t]=n[t]);const e=i(n,o),r=new Set;if(typeof e.path<"u"&&(e.path instanceof Array||e.path instanceof Set))for(const t of e.path)r.add(t);return{name:"astro-dead-links",hooks:{"astro:build:done":async({dir:t})=>{r.size||r.add(t)}}}};export { s as default };
+
