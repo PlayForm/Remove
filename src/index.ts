2058159db6bdb0101bd601f536e498aa8766855d
Nikola Hristov <nikola@nikolahristov.tech>
2023-07-30 01:52:54 +0300

    squash!

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import type { AstroIntegration } from "astro";
 import deepmerge from "files-pipe/dist/lib/deepmerge.js";
 import type { optionPath } from "files-pipe/dist/options/index.js";
+
 import type { Options } from "./options/index.js";
 import defaults from "./options/index.js";
 
