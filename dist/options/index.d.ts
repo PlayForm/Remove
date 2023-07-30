94c1229d59080aec043c0b33255bfd974ae87f2e
Nikola Hristov <nikola@nikolahristov.tech>
2023-07-30 20:16:11 +0300

    squash!

--- dist/options/index.d.ts
+++
@@ -1,6 +0,0 @@
-import type { Options as OptionsBase } from "files-pipe/dist/options/index.js";
-export interface Options extends OptionsBase {
-    [key: string]: any;
-}
-declare const _default: Options;
-export default _default;
