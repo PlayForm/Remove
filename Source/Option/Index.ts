fa5fa813ab1c353d0611656ad87c86cc6f4e335c
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-14 07:04:33 +0300

    squash!

--- Source/Option/Index.ts
+++ Source/Option/Index.ts
@@ -1,12 +1,12 @@
 import Merge from "files-pipe/Target/Library/Merge.js";
 import type { Options as OptionsBase } from "files-pipe/Target/Options/Index.js";
-import Defaults from "files-pipe/Target/Options/Index.js";
+import Default from "files-pipe/Target/Options/Index.js";
 
 export interface Options extends OptionsBase {
 	// rome-ignore lint/suspicious/noExplicitAny:
 	[key: string]: any;
 }
 
-export default Merge(Defaults, {
+export default Merge(Default, {
 	Pipe: {},
 } satisfies Options) as Options;
