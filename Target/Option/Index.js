6cfe0c9d4dbea46281673e459b72d6757858d7ee
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-17 19:52:21 +0300

    squash!

--- Target/Option/Index.js
+++ Target/Option/Index.js
@@ -1,1 +1,1 @@
-import{Merge as t}from"files-pipe";import e from"files-pipe/Target/Option/Index.js";var o=t(e,{Pipe:{}});export{o as default};
+import{Default as t,Merge as e}from"files-pipe";var p=e(t,{Pipe:{}});export{p as default};
