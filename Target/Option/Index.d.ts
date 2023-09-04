341108e46cd777bcc65c3a4e3767c68fbedd9196
Nikola Hristov <nikola@nikolahristov.tech>
2023-09-04 22:14:02 +0300

    squash!

---
+++ Target/Option/Index.d.ts
@@ -0,0 +1,6 @@
+import type { Option as _Option } from "files-pipe";
+export interface Option extends _Option {
+    [key: string]: any;
+}
+declare const _default: Option;
+export default _default;
---
+++ Target/Option/Index.js
@@ -0,0 +1,1 @@
+import{Default as t,Merge as e}from"files-pipe";var p=e(t,{Pipe:{}});export{p as default};
