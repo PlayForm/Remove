7329e7e732172296b693b8a537510505a9fb218d
Nikola Hristov <nikola@nikolahristov.tech>
2024-01-01 22:22:05 +0200

    squash!

--- Target/Function/Integration.d.ts
+++ Target/Function/Integration.d.ts
@@ -5,52 +5,23 @@
 declare const _default: Type;
 export default _default;
 import type Type from "../Interface/Integraton.js";
-export declare const Default: Omit<
-	{} & {
-		Cache: {
-			Search: string;
-			Folder: string;
-		};
-		Path: string;
-		Logger: 2;
-		Action: Omit<
-			{} & {
-				Read: ({
-					Input,
-				}: import("files-pipe/Target/Interface/File.js").default) => Promise<string>;
-				Wrote: ({
-					Buffer,
-				}: import("files-pipe/Target/Interface/File.js").default) => Promise<
-					import("files-pipe/Target/Type/Buffer.js").Type
-				>;
-				Passed: (
-					On: import("files-pipe/Target/Interface/File.js").default,
-				) => Promise<true>;
-				Failed: ({
-					Input,
-				}: import("files-pipe/Target/Interface/File.js").default) => Promise<string>;
-				Accomplished: ({
-					Input,
-					Output,
-				}: import("files-pipe/Target/Interface/File.js").default) => Promise<string>;
-				Fulfilled: ({
-					Files,
-				}: import("files-pipe/Target/Interface/Plan.js").default) => Promise<
-					string | false
-				>;
-				Changed: (
-					Plan: import("files-pipe/Target/Interface/Plan.js").default,
-				) => Promise<
-					import("files-pipe/Target/Interface/Plan.js").default
-				>;
-			},
-			"__proto__"
-		>;
-		Files: string;
-		Exclude: false;
-	},
-	"__proto__"
->;
-export declare const Merge: import("typescript-esbuild/Target/Interface/Merge.js").default<
-	import("typescript-esbuild/Target/Interface/Merge.js").Generic
->;
+export declare const Default: Omit<{} & {
+    Cache: {
+        Search: string;
+        Folder: string;
+    };
+    Path: string;
+    Logger: 2;
+    Action: Omit<{} & {
+        Read: ({ Input }: import("files-pipe/Target/Interface/File.js").default) => Promise<string>;
+        Wrote: ({ Buffer }: import("files-pipe/Target/Interface/File.js").default) => Promise<import("files-pipe/Target/Type/Buffer.js").Type>;
+        Passed: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<true>;
+        Failed: ({ Input }: import("files-pipe/Target/Interface/File.js").default) => Promise<string>;
+        Accomplished: ({ Input, Output }: import("files-pipe/Target/Interface/File.js").default) => Promise<string>;
+        Fulfilled: ({ Files }: import("files-pipe/Target/Interface/Plan.js").default) => Promise<string | false>;
+        Changed: (Plan: import("files-pipe/Target/Interface/Plan.js").default) => Promise<import("files-pipe/Target/Interface/Plan.js").default>;
+    }, "__proto__">;
+    Files: string;
+    Exclude: false;
+}, "__proto__">;
+export declare const Merge: import("typescript-esbuild/Target/Interface/Merge.js").default<import("typescript-esbuild/Target/Interface/Merge.js").Generic>;
--- Target/Variable/Option.d.ts
+++ Target/Variable/Option.d.ts
@@ -2,50 +2,23 @@
  * @module Option
  *
  */
-declare const _default: Omit<
-	{} & {
-		Cache: {
-			Search: string;
-			Folder: string;
-		};
-		Path: string;
-		Logger: 2;
-		Action: Omit<
-			{} & {
-				Read: ({
-					Input,
-				}: import("files-pipe/Target/Interface/File").default) => Promise<string>;
-				Wrote: ({
-					Buffer,
-				}: import("files-pipe/Target/Interface/File").default) => Promise<
-					import("files-pipe/Target/Type/Buffer").Type
-				>;
-				Passed: (
-					On: import("files-pipe/Target/Interface/File").default,
-				) => Promise<true>;
-				Failed: ({
-					Input,
-				}: import("files-pipe/Target/Interface/File").default) => Promise<string>;
-				Accomplished: ({
-					Input,
-					Output,
-				}: import("files-pipe/Target/Interface/File").default) => Promise<string>;
-				Fulfilled: ({
-					Files,
-				}: import("files-pipe/Target/Interface/Plan").default) => Promise<
-					string | false
-				>;
-				Changed: (
-					Plan: import("files-pipe/Target/Interface/Plan").default,
-				) => Promise<
-					import("files-pipe/Target/Interface/Plan").default
-				>;
-			},
-			"__proto__"
-		>;
-		Files: string;
-		Exclude: false;
-	},
-	"__proto__"
->;
+declare const _default: Omit<{} & {
+    Cache: {
+        Search: string;
+        Folder: string;
+    };
+    Path: string;
+    Logger: 2;
+    Action: Omit<{} & {
+        Read: ({ Input }: import("files-pipe/Target/Interface/File").default) => Promise<string>;
+        Wrote: ({ Buffer }: import("files-pipe/Target/Interface/File").default) => Promise<import("files-pipe/Target/Type/Buffer").Type>;
+        Passed: (On: import("files-pipe/Target/Interface/File").default) => Promise<true>;
+        Failed: ({ Input }: import("files-pipe/Target/Interface/File").default) => Promise<string>;
+        Accomplished: ({ Input, Output }: import("files-pipe/Target/Interface/File").default) => Promise<string>;
+        Fulfilled: ({ Files }: import("files-pipe/Target/Interface/Plan").default) => Promise<string | false>;
+        Changed: (Plan: import("files-pipe/Target/Interface/Plan").default) => Promise<import("files-pipe/Target/Interface/Plan").default>;
+    }, "__proto__">;
+    Files: string;
+    Exclude: false;
+}, "__proto__">;
 export default _default;
