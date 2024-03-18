f5ad50f8f7a70a40238e1709b0d01f06a0882f38
Nikola R. Hristov <Nikola@Playform.Cloud>
2024-03-19 01:07:09 +0200

    squash!

--- Documentation/assets/navigation.js
+++
@@ -1,2 +0,0 @@
-window.navigationData =
-	"data:application/octet-stream;base64,H4sIAAAAAAAACqXRTwuCMBgG8O/ynkeBloedI+ggXaKLSCx91dGaMiYU4XcP/6DbUiK6bs9+PO+76AUaHxooHKTGXDHNSwkEKqYLoBCWaS1wbdytCn0XQODGZQrUI5AUXKQKJdBotE7PCiekfa0ylthOm3GwbdCQ0dhhxmqhJ+bMFGdXp82QsiHfM5zZufa1TNoTy1qcMtgYXogqxy9Sl/kwYqdVv5NjtbjyLnDpA3/v3cLml28WXKj1QxlXGP9vMOYo32viJn4DSpHFO5UCAAA=";
