597018f5065c2b6eca8e8ba57cb4c87df7d0946b
Nikola R. Hristov <nikola@nikolahristov.tech>
2024-03-31 21:17:53 +0300

    squash!

--- Source/Variable/Option.ts
+++ Source/Variable/Option.ts
@@ -4,6 +4,6 @@
  */
 export default (
 	await import("typescript-esbuild/Target/Function/Merge.js")
-).default((await import("files-pipe/Target/Variable/Option.js")).default, {
+).default((await import("@playform/file-pipe/Target/Variable/Option.js")).default, {
 	Action: {},
 });
